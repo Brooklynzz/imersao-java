99a0814e6c66cbb691f9f329a6d0f81b6c8e12a7
Marcus Vinicius <terrences2u@gmail.com>
2023-03-30 17:32:02 -0300

    feat: parser

--- moviesAPI/src/App.java
+++ moviesAPI/src/App.java
@@ -2,9 +2,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Conexão HTTP para requisitar os filmes
+
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -13,6 +17,18 @@ public class App {
         HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
 
-        System.out.println(body);
+        // Parsear String
+
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // Exibir dados
+
+        for (Map<String,String> filme: listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
