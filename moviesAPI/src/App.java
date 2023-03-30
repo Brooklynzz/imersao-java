a7c71bd211cbf10f60e8d0c7d26c28e0ec996a08
Marcus Vinicius <terrences2u@gmail.com>
2023-03-30 16:33:25 -0300

    initial commit

---
+++ moviesAPI/src/App.java
@@ -0,0 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
+    }
+}
