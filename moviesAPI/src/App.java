afe87133856e88a092c456848bbf517136edc604
Marcus Vinicius <terrences2u@gmail.com>
2023-04-03 01:35:45 -0300

    feat: added stickersgen to app

--- moviesAPI/src/App.java
+++ moviesAPI/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,7 +10,6 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
         // Conexão HTTP para requisitar os filmes
-
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -18,17 +19,20 @@ public class App {
         String body = response.body();
 
         // Parsear String
-
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // Exibir dados
-
+        var stickers = new StickersGen();
         for (Map<String,String> filme: listaDeFilmes) {
+            String urlImagem= filme.get("image");
+            InputStream inputStream = new URL(urlImagem).openStream();
+
             System.out.println("Título: " + filme.get("title"));
-            System.out.println("Pôster: " + filme.get("image"));
+            System.out.println();
             System.out.println("Classificação: " + filme.get("imDbRating"));
             System.out.println();
+            stickers.cria(inputStream, filme.get("title"));
         }
     }
 }
--- moviesAPI/src/StickersGen.java
+++ moviesAPI/src/StickersGen.java
@@ -2,15 +2,15 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 public class StickersGen {
-    public void cria() throws Exception {
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
         // Leitura da imagem
-
-        BufferedImage imagemOriginal = ImageIO.read(new File("media/TopMovies_1.jpg"));
+//        InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // Criar nova imagem em memória com tamanho novo e transparência
-
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int novaAltura = altura + 200;
@@ -20,15 +20,16 @@ public class StickersGen {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal,0,0, null );
 
+        // Configurar a fonte
+        Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.yellow);
+
         // Escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA",200, novaAltura - 100);
 
         // Escrever a imagem em um arquivo
-        ImageIO.write(novaImagem, "png", new File("media/figurinha.png"));
+        ImageIO.write(novaImagem, "png", new File("media/saida/" + nomeArquivo + ".png"));
 
-    }
-
-    public static void main(String[] args) throws Exception {
-        StickersGen stickers = new StickersGen();
-        stickers.cria();
     }
 }
